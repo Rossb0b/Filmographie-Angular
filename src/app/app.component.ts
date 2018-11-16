c0bbb6a67fe7e6c89aa72c4ca07cb21c10993872
Nicolas Hallaert <nicolas.hallaert.pro@gmail.com>
2018-11-16 09:25:59 +0100

    Desactivated typed.js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Typed } from './typed.js';
+// import { Typed } from './typed.js';
 
 @Component({
   selector: 'app-root',
@@ -11,15 +11,15 @@ export class AppComponent implements OnInit {
   title = 'Filmographie - Angular';
 
   ngOnInit() {
-    let options = {
-      strings: ["Bienvenue sur ma Filmographie", "Celle-ci est développée avec Angular 6"],
-      typeSpeed:200,
-      backSpeed:20,
-      showCursor: true,
-      cursorChar: "",
-      loop:true
-    }
-
-    let typed = new Typed(".typing-element", options);
+  //   let options = {
+  //     strings: ["Bienvenue sur ma Filmographie", "Celle-ci est développée avec Angular 6"],
+  //     typeSpeed:200,
+  //     backSpeed:20,
+  //     showCursor: true,
+  //     cursorChar: "",
+  //     loop:true
   }
+
+  //   let typed = new Typed(".typing-element", options);
+  // }
 }
